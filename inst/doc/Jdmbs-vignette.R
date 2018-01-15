9508519ee789ae0d701eb1d666766c47de5c9ce1
Masashi OKADA <okadaalgorithm@gmail.com>
2018-01-15 14:30:49 +0900

    Add files via upload
    changed documents.

--- inst/doc/Jdmbs-vignette.R
+++ inst/doc/Jdmbs-vignette.R
@@ -1,4 +1,4 @@
-## ----fig.width=3.5,fig.height=3.5,fig.align="center",echo=FALSE----------
+## ----fig.width=3.1,fig.height=3.1,fig.align="center",echo=FALSE----------
 library(png)
 library(grid)
 img <- readPNG("./companies.png")
