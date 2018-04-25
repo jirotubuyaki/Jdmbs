332b0db687189c855ce083bd18453ed88a5bdf47
Masashi OKADA <okadaalgorithm@gmail.com>
2018-04-26 00:47:19 +0900

    Add files via upload
    version 1.2

--- inst/doc/Jdmbs-vignette.R
+++ inst/doc/Jdmbs-vignette.R
@@ -1,12 +1,12 @@
-## ----fig.width=3.1,fig.height=3.1,fig.align="center",echo=FALSE----------
+## ----fig.width=2.2,fig.height=2.2,fig.cap="The relation of companies",echo=FALSE----
 library(png)
 library(grid)
 img <- readPNG("./companies.png")
  grid.raster(img)
 
-## ---- echo=FALSE, fig.width=60, fig.align='center', fig.height=25, dpi=300,out.width='16.5cm'----
-load("../data/data.rda")
-source('../R/Jdmbs.R')
-normal_bs(3 ,simulation.length=90,monte_carlo=80, c(10000,7000,5000), c(0.0012, 0.0012, 0.0012),
-+ c(sqrt(0.002),sqrt(0.002),sqrt(0.002)), c(2500,3000,1500), c("deeppink","royalblue","greenyellow"))
+## ----fig.width=15.0,fig.height=6.0,fig.cap="Simulation of geometric Brownian motion",echo=FALSE----
+library(png)
+library(grid)
+img <- readPNG("./image.png")
+ grid.raster(img)
 
